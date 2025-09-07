baeb5aa881bdd93ce6cd9727d20787106b31e4e0
Lenin Mombe <leninmombe91@gmail.com>
2025-09-07 22:45:48 +0200

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,12 +1,10 @@
-# relationship_app/views.py
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.contrib.auth.decorators import user_passes_test
+from .models import UserProfile
 
-# Helper function to check if the user is an Admin
-def is_admin(user):
-    return user.userprofile.role == 'Admin'
-
-# Admin view that is accessible only by users with 'Admin' role
-@user_passes_test(is_admin)
+# Admin view - Only accessible by users with 'Admin' role
+@user_passes_test(lambda user: user.userprofile.role == 'Admin')
 def admin_view(request):
-    return render(request, 'relationship_app/admin_view.html')
+    return render(request, 'admin_view.html')
