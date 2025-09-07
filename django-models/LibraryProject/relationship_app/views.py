429f84efbb9453c6c8276a3e49c16b933aa9460a
Lenin Mombe <leninmombe91@gmail.com>
2025-09-07 22:20:14 +0200

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,9 +1,9 @@
 # relationship_app/views.py
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic import DetailView  # Correct import for DetailView
 from .models import Library, Book
 
-# Function-based view (Already added for listing books)
+# Function-based view for listing all books
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
@@ -12,11 +12,10 @@ def list_books(request):
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
-    context_object_name = 'library'  # To access the object in the template as 'library'
+    context_object_name = 'library'  # To access the object as 'library' in the template
     
-    # Optionally, you can define a custom get_context_data to include additional data
+    # Optionally, you can define a custom get_context_data method to add more context
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        # Add books related to this specific library
-        context['books'] = self.object.books.all()  # Assuming 'books' is a ManyToMany relationship
+        context['books'] = self.object.books.all()  # Fetch all books for this library
         return context
