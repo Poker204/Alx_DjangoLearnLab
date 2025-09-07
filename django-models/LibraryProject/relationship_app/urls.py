0fada94efbbea9bd50177dd6944475ed91136689
Lenin Mombe <leninmombe91@gmail.com>
2025-09-07 22:33:18 +0200

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -9,4 +9,4 @@ urlpatterns = [
     
     # URL for the class-based view that shows details of a specific library
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
-]
+views.register", "LogoutView.as_view(template_name=", "LoginView.as_view(template_name="]
