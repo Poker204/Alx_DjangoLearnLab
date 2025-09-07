03d4aa52ae2ba147202e8c15bf49df6e4b43694e
Lenin Mombe <leninmombe91@gmail.com>
2025-09-07 22:42:59 +0200

    Update models.py

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -2,7 +2,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Role choices for the UserProfile model
 ROLE_CHOICES = [
     ('Admin', 'Admin'),
     ('Librarian', 'Librarian'),
