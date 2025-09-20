599fbfdd477d5f1a41224dd78b12d607161a2d1f
Lenin Mombe <leninmombe91@gmail.com>
2025-09-20 13:26:40 +0200

    Update settings.py

--- advanced_features_and_security/settings.py
+++ advanced_features_and_security/settings.py
@@ -5,6 +5,7 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'LibraryProject.bookshelf',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
