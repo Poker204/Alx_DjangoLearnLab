c1b1f66a75e81b3a4158f3b7d3423c8c705747f5
Lenin Mombe <leninmombe91@gmail.com>
2025-09-21 21:45:07 +0200

    Create models.py

---
+++ advanced_features_and_security/LibraryProject/models.py
@@ -0,0 +1,11 @@
+# LibraryProject/bookshelf/models.py
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    published_date = models.DateField(null=True, blank=True)
+    isbn = models.CharField(max_length=13, unique=True)
+
+    def __str__(self):
+        return self.title
