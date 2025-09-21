d830402ed277cd8f224fa7f5daf3a2e6b07607f4
Lenin Mombe <leninmombe91@gmail.com>
2025-09-21 21:50:15 +0200

    Create settings.py

---
+++ advanced_features_and_security/LibraryProject/settings.py
@@ -0,0 +1,12 @@
+# LibraryProject/LibraryProject/settings.py
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'bookshelf',  # custom user app
+]
+
+AUTH_USER_MODEL = 'bookshelf.CustomUser'
