a908a8627b97ab63cddfb53248f44530087903b5
Lenin Mombe <leninmombe91@gmail.com>
2025-09-21 22:23:26 +0200

    Update settings.py

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -110,7 +110,7 @@ CSRF_COOKIE_SECURE = True
 X_FRAME_OPTIONS = "DENY"                 # Prevent clickjacking
 SECURE_CONTENT_TYPE_NOSNIFF = True       # Prevent MIME-type sniffing
 SECURE_BROWSER_XSS_FILTER = True         # Enable browser XSS filter
-
+SECURE_PROXY_SSL_HEADER", "HTTP_X_FORWARDED_PROTO", "https
 # ------------------------
 # Logging (Optional for Security Review)
 # ------------------------
