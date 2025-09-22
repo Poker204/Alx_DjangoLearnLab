e936ddf2fc171bcd7a8d97be504e1293c7386df3
Lenin Mombe <leninmombe91@gmail.com>
2025-09-22 09:09:53 +0200

    Create urls.py

---
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -0,0 +1,11 @@
+# advanced_features_and_security/LibraryProject/relationship_app/urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # Function-based view
+    path('example/', views.example_function_view, name='example-function'),
+
+    # Class-based view
+    path('example-class/', views.ExampleClassView.as_view(), name='example-class'),
+]
