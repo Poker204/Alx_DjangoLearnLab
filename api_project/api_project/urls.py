581dc3256892002a7123f568550c88687e18ef9d
Lenin Mombe <leninmombe91@gmail.com>
2025-09-22 07:58:46 +0200

    Update urls.py

--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),   # ✅ include your app’s URLs
+    path('api/', include('api.urls')),  # Your API routes
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),  # Token login
 ]
