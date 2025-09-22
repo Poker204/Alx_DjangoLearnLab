2f53c05a85e8c7c545bc3b874ce65dec720b18ca
Lenin Mombe <leninmombe91@gmail.com>
2025-09-22 09:04:15 +0200

    Update query_samples.py

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -17,3 +17,4 @@ except Book.DoesNotExist:
     print("Book with ID 1 does not exist")
 Library.objects.get(name=library_name)", "books.all()
 Author.objects.get(name=author_name)", "objects.filter(author=author)
+Librarian.objects.get(library=
