c5d4828ab3003353c0cc484ed8aa3a05e955a9c9
Lenin Mombe <leninmombe91@gmail.com>
2025-09-22 07:59:29 +0200

    Update views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,13 +1,14 @@
 from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from .models import Book
 from .serializers import BookSerializer
 
-# Existing ListAPIView
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]  # Only logged-in users can list books
 
-# New ViewSet for full CRUD
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]  # Secure all CRUD endpoints
