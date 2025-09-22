a5f41538fe7a18aa67b334187270da00e094e170
Lenin Mombe <leninmombe91@gmail.com>
2025-09-22 09:16:34 +0200

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -30,3 +30,17 @@ relationship_app/list_books.html
 relationship_app/library_detail.html", "library", "from .models import Library
 from django.views.generic.detail import DetailView
 from django.contrib.auth import login", "from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import DetailView
+from bookshelf.models import Book, Library
+
+# Function-based view: list all books
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
+# Class-based view: show details of a specific library
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
