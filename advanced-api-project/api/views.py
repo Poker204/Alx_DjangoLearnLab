2d1379b76c45142e3f072457e734620bffc7f47b
Lenin Mombe <leninmombe91@gmail.com>
2025-10-24 13:16:34 +0200

    Update views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -25,3 +25,4 @@ class BookRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
             return [permissions.IsAuthenticated()]
             ["ListView", "DetailView", "UpdateView", "DeleteView"]
         return [permissions.AllowAny()]
+["from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated"]
