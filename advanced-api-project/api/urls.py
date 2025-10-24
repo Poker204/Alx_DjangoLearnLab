577730acc972bc122ea24456878b79017e0d92f8
Lenin Mombe <leninmombe91@gmail.com>
2025-10-24 13:13:56 +0200

    Update urls.py

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -4,4 +4,6 @@ from .views import BookListCreateView, BookRetrieveUpdateDestroyView
 urlpatterns = [
     path('books/', BookListCreateView.as_view(), name='book-list-create'),
     path('books/<int:pk>/', BookRetrieveUpdateDestroyView.as_view(), name='book-detail'),
+     path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
 ]
